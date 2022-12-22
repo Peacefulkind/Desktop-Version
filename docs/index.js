68de11e2eb9d21844ec0ec07e5fae1da60e89a1b
Peacefulkind <sharingforexideas@gmail.com>
2022-12-22 17:32:52 +0300

    Add local storage

--- docs/index.js
+++ docs/index.js
@@ -198,3 +198,33 @@ for (let i = 0; i < arrWork.length; i++) {
       `;
   });
 }
+
+function validate() {
+  const emailVal = document.getElementById('email').value;
+  const regx = /^([a-z\d-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+  // eslint-disable-next-line func-names
+  document.forms[0].onsubmit = function (e) {
+    if (regx.test(emailVal)) {
+      document.querySelector('small').innerHTML = 'Valid';
+      document.querySelector('small').style.color = 'green';
+    } else {
+      document.querySelector('small').innerHTML = 'Invalid! Please enter the email in lower case';
+      document.querySelector('small').style.color = 'red';
+      e.preventDefault();
+    }
+  };
+}
+
+form.addEventListener('input', () => {
+  const contactData = {
+    username: userName.value,
+    email: Email.value,
+    message: Msg.value,
+  };
+  localStorage.setItem('contact', JSON.stringify(contactData));
+});
+
+const getLocalStorageData = JSON.parse(localStorage.getItem('contact'));
+userName.value = getLocalStorageData.username;
+Email.value = getLocalStorageData.email;
+Msg.value = getLocalStorageData.message
