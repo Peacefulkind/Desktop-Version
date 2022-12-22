9a632bb15977ee777c18d6c1dbe8bd2d7cf7474c
Fene-87 <83401478+Fene-87@users.noreply.github.com>
2022-12-22 01:53:23 -0900

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-plusplus */
 /* eslint-disable no-unused-vars */
 
+const humburgerBtn = document.querySelector('.humburger');
 const navMenu = document.querySelector('.nav-menue');
 const Portfolio = document.querySelectorAll('.card');
 const Modal = document.querySelector('.modal-container');
