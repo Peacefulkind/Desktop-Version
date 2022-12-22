6fa090e9088cd14bc9eb53bffa4782115c54c91c
Peacefulkind <sharingforexideas@gmail.com>
2022-12-22 14:40:25 +0300

    Remove disabled linters as instructed

--- docs/index.js
+++ docs/index.js
@@ -1,6 +1,3 @@
-/* eslint-disable no-alert */
-/* eslint-disable no-plusplus */
-/* eslint-disable no-unused-vars */
 const humburgerBtn = document.querySelector('.humburger');
 const navMenu = document.querySelector('.nav-menue');
 const Portfolio = document.querySelectorAll('.card');
